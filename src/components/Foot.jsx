f98142e3db9f72ca88f3a3d05b709736e4d48078
Shreyans-Nawlakha <86418929+Shreyans-Nawlakha@users.noreply.github.com>
2023-07-05 18:22:43 +0530

    footer

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -6,12 +6,12 @@ export default function Foot() {
   return (
     <footer className="relative w-full" id="footer">
       <div className="mx-auto w-full max-w-7xl px-8">
-        <div className="mt-12 flex w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
+        <div className="mt-12 flex w-full flex-col items-center justify-center border-t border-blue-gray-800 py-4 md:flex-row md:justify-between">
           <Typography
             variant="small"
             className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
           >
-            &copy; {currentYear} <a href="https://material-tailwind.com/">Material Tailwind</a>. All
+            &copy; {currentYear} Gift Daddy. All
             Rights Reserved.
           </Typography>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
